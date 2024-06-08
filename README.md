da5a8408627dd91e397567925ea67a066a55b47a
cdubos-fr <contact@cdubos.fr>
2024-06-08 19:48:51 +0000

    fix: work without --impure parameter

---
+++ README.md
@@ -0,0 +1,18 @@
+My personnal NixOS configuration file
+
+
+Install
+=======
+
+1. WSL
+
+```console
+sudo nixos-rebuild switch --flake github:cdubos-fr/nixconfig#destiny-wsl
+```
+
+
+2. NixOS
+
+```console
+sudo nixos-rebuild switch --flake github:cdubos-fr/nixconfig#destiny
+```
--- modules/nixos/wsl.nix
+++ modules/nixos/wsl.nix
@@ -1,7 +1,10 @@
 { config, lib, pkgs, ... }:
 {
     imports = [
-        (fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master")
+        (fetchTarball {
+            url="https://github.com/msteen/nixos-vscode-server/tarball/master";
+            sha256 = "sha256:1rq8mrlmbzpcbv9ys0x88alw30ks70jlmvnfr2j8v830yy5wvw7h";
+        })
     ];
     programs.nix-ld.enable = true;
 
